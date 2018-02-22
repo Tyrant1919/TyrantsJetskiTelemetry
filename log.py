3b7725253c3b14e842de17d182e1178b5b74191a
Tyrant1919 <33078756+Tyrant1919@users.noreply.github.com>
2018-02-21 22:59:37 -0500

    Update log.py

--- log.py
+++ log.py
@@ -7,9 +7,9 @@ serialport = serial.Serial("/dev/ttyUSB0", 115200) #\\used to be ttyAMA0
 try:
         line = (serialport.readline())
         while True:
-                sum,x,y,z = (serialport.readline()).split('|')
+                time,diff,sum,x,y,z = (serialport.readline()).split('|')
                 conn = sqlite3.connect('/var/www/html/testski.db')
-                conn.execute('INSERT INTO ski VALUES(?,?,?,?)',(sum,x,y,z))
+                conn.execute('INSERT INTO ski VALUES(?,?,?,?,?,?)',(time,diff,sum,x,y,z))
                 conn.commit()
 except KeyboardInterrupt:
         conn.close()
